58a390de44fe515802c7afe849f27e6ec007876a
Ng Chin How <ngchinhow@gmail.com>
2023-04-07 20:06:24 +0800

    Changed WebsocketsAuthenticationClientTest a bit.

--- src/test/java/io/github/ngchinhow/kraken/rest/WebsocketsAuthenticationClientTest.java
+++ src/test/java/io/github/ngchinhow/kraken/rest/WebsocketsAuthenticationClientTest.java
@@ -5,24 +5,18 @@ import io.github.ngchinhow.kraken.rest.client.WebSocketsAuthenticationClient;
 import io.github.ngchinhow.kraken.rest.model.KrakenResponse;
 import io.github.ngchinhow.kraken.rest.model.websocketsauthentication.WebSocketsToken;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class WebsocketsAuthenticationClientTest {
 
-    private WebSocketsAuthenticationClient webSocketsAuthenticationClient;
-
-    @BeforeEach
-    public void beforeEach() {
-        this.webSocketsAuthenticationClient = new KrakenConnectionManager(
-            System.getenv("KRAKEN_REST_API_KEY"),
-            System.getenv("KRAKEN_REST_PRIVATE_KEY")
-        ).getRestClient(WebSocketsAuthenticationClient.class);
-    }
+    private static final WebSocketsAuthenticationClient WEB_SOCKETS_AUTHENTICATION_CLIENT = new KrakenConnectionManager(
+        System.getenv("KRAKEN_REST_API_KEY"),
+        System.getenv("KRAKEN_REST_PRIVATE_KEY")
+    ).getRestClient(WebSocketsAuthenticationClient.class);
 
     @Test
     public void givenAPICredentials_whenGetWebSocketsToken_thenSucceed() {
-        KrakenResponse<WebSocketsToken.Result> response = webSocketsAuthenticationClient.getWebsocketsToken();
+        KrakenResponse<WebSocketsToken.Result> response = WEB_SOCKETS_AUTHENTICATION_CLIENT.getWebsocketsToken();
         Assertions.assertNotNull(response);
         Assertions.assertTrue(response.getError().isEmpty());
         Assertions.assertNotNull(response.getResult().getToken());
